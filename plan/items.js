70b83a92d91bd3bd28cbf809c49bef1f9e55640b
LRache <2746324475@qq.com>
2024-05-14 22:21:41 +0800

    fixed submit bug

--- plan/items.js
+++ plan/items.js
@@ -1,4 +1,13 @@
-document.getElementById("newItemForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    console.log("SUBMIT");
+document.getElementById("newItemForm").addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    let formData = new FormData(this)
+    fetch('https://localhost:8080/tripplan', {
+        method: 'POST',
+        body: formData
+    })
+        .then(function (response) {
+            if (response.status === 201) {
+                alert("添加成功")
+            }
+        })  // 假设服务器返回JSON数据// 处理返回的数据
 })
